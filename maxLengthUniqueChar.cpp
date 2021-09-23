0c06aa7cbd5f3a8c2de487dc4affd559cc04f925
nolayercode <vyelhekar@gmail.com>
2021-09-23 23:44:44 +0530

    added RomanToInt python code

---
+++ maxLengthUniqueChar.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxLength(vector<string> &arr)
+    {
+        // vector<string>::iterator it;
+        int maxLengthCount = 0;
+        // set_union(string1.begin(), string1.end(), string2.begin(), string2.end(), std::back_inserter(string_union));
+        
+        for (int i = 0; i !=)
+            return maxLengthCount;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<string> temp = {"un", "iq", "ue"};
+    Solution S;
+    cout << S.maxLength(temp);
+    return 0;
+}
