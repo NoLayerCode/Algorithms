2774237afd5707cdfaa79afecf691bdc7ea31a89
nolayercode <vyelhekar@gmail.com>
2022-10-03 23:20:12 +0530

    removed comment 1578. Minimum Time to Make Rope Colorful (leetcode)

--- colourFuleRope.py
+++ colourFuleRope.py
@@ -5,7 +5,6 @@ def minCost(colors: str, neededTime: list[int]) -> int:
 	cost = 0
 	for index in range(1, n):
 		if colors[index - 1] == colors[index]:
-			# cost = cost+neededTime[index] if neededTime[index] < neededTime[index+1]  else cost+neededTime[index+1]
 			cost = cost+min(neededTime[index-1], neededTime[index])
 			if neededTime[index-1] > neededTime[index]:
 				neededTime[index-1], neededTime[index] = neededTime[index],  neededTime[index-1]
